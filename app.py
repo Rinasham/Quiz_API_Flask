8f666aff71e41dd70a3d98a9ed25a23bcfd059e3
Rinasham <rinashamau@gmail.com>
2022-04-21 05:35:40 +1000

    [fix] get request to API

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import psycopg2
 from flask import Flask, request, redirect, render_template
+import requests
 import settings
 
 
 app = Flask(__name__)
 
 key = settings.AP
-
+print(key)
 @app.route('/')
 def index():
     return render_template('home.html')
 
 @app.route('/quiz')
 def quiz_top():
-    url = f'https://quizapi.io/api/v1/questions?apiKey={key}'
+    url = 'http://localhost:3000/quiz/sql'
+    res = requests.get(url).json()
+    print(res)
+
     return render_template('quiz.html')
 
 
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv(override=True)
 
 
 # set API key in .env as an enviroment eariable
